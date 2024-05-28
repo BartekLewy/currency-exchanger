97ba04070e921cb20f55a39767fc9a566db93201
Bartosz Lewandowski <bartosz.dev@yahoo.com>
2024-05-29 01:17:40 +0200

    chore: remove unused imports from tests classes

--- tests/WithPurchaseFeeCalculatorTest.php
+++ tests/WithPurchaseFeeCalculatorTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Bartosz\CurrencyExchanger\Tests;
 
 use Bartosz\CurrencyExchanger\Currency;
-use Bartosz\CurrencyExchanger\Money;
 use Bartosz\CurrencyExchanger\Tests\ObjectMothers\MoneyObjectMother;
 use Bartosz\CurrencyExchanger\Tests\TestDoubles\FakeExchangeCalculator;
 use Bartosz\CurrencyExchanger\WithPurchaseFeeCalculator;
